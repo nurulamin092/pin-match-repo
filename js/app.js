5c16d8767add506d7e423e2d6c07fe52be6f90e2
nurulamin092 <mnurulamin092@gmial.com>
2022-02-14 12:56:48 +0600

    keypad add

--- js/app.js
+++ js/app.js
@@ -11,8 +11,25 @@ function getPin() {
     }
 
 }
-
 function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calInput.value = '';
+        }
+        console.log(number);
+    }
+    else {
+        const previousNumber = calInput.value;
+        const newNumber = previousNumber + number;
+        calInput.value = newNumber;
+    }
+
+
+});
